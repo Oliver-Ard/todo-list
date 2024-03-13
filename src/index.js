5ac0a33ae14123d16d740750a033ea5345f89298
Oliver Ardelean <148954618+Oliver-Ard@users.noreply.github.com>
2024-03-13 17:32:57 +0200

    Create and design the sidebar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./main.css";
+
+class App {
+	constructor() {
+		this.#loadEventListeners();
+	}
+
+	#toggleSidebar() {
+		const sidebar = document.querySelector("[data-element = 'sidebar']");
+		sidebar.classList.toggle("active");
+	}
+
+	#loadEventListeners() {
+		const sliderBtn = document.querySelector("[data-element = 'slider-btn']");
+
+		sliderBtn.addEventListener("click", this.#toggleSidebar);
+	}
+}
+
+const app = new App();
