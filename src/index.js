512d5937f609ead873516fb1ab637396497df121
Oliver Ardelean <148954618+Oliver-Ard@users.noreply.github.com>
2024-03-15 20:17:38 +0200

    Add remove todo feature

--- src/index.js
+++ src/index.js
@@ -20,29 +20,11 @@ class App {
 		switch (sectionName) {
 			case "inbox": {
 				this.#display.renderInbox();
-				this.#handleAddTaskForm();
+				this.#addTask();
 				this.#showTodosList();
 				break;
 			}
 		}
-	}
-
-	#handleAddTaskForm() {
-		const addTaskForm = document.querySelector("[data-form = 'add-task']");
-
-		addTaskForm.addEventListener("submit", () => {
-			// Get Elements
-			const taskName = document.querySelector("#task-name").value;
-			const taskDescription = document.querySelector("#task-description").value;
-			const dueDate = document.querySelector("#due-date").value;
-			const priorityStatus = document.querySelector("#priority-status").value;
-			// Add Task
-			const task = new Todo(taskName, taskDescription, dueDate, priorityStatus);
-
-			this.todos.addTodo(task);
-			this.#showTodosList();
-			addTaskForm.reset();
-		});
 	}
 
 	#showTodosList() {
@@ -56,9 +38,37 @@ class App {
 				todo.dueDate,
 				todo.priorityStatus
 			);
+			// Add index for each todo
+			todoEl.setAttribute("data-index", `${this.todos.list.indexOf(todo)}`);
 
 			todosListEl.append(todoEl);
 		}
+	}
+
+	#addTask() {
+		const addTaskForm = document.querySelector("[data-form = 'add-task']");
+
+		addTaskForm.addEventListener("submit", () => {
+			// Get Elements
+			const taskName = document.querySelector("#task-name").value;
+			const taskDescription = document.querySelector("#task-description").value;
+			const dueDate = document.querySelector("#due-date").value;
+			const priorityStatus = document.querySelector("#priority-status").value;
+			// Add Task
+			const task = new Todo(taskName, taskDescription, dueDate, priorityStatus);
+
+			this.todos.addTodo(task);
+			addTaskForm.reset();
+
+			this.#showTodosList();
+		});
+	}
+
+	#deleteTask(targetItem) {
+		const taskToDelete = targetItem.parentNode.dataset.index;
+		this.todos.removeTodo(taskToDelete);
+
+		this.#showTodosList();
 	}
 
 	// HELPER METHODS
@@ -87,6 +97,10 @@ class App {
 		switch (target.dataset.button || targetParent.dataset.button) {
 			case "add": {
 				this.#toggleModal("add-task");
+				break;
+			}
+			case "delete-todo": {
+				this.#deleteTask(targetParent);
 				break;
 			}
 			case "close-modal": {
